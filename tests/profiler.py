6e50284c2ce8cc50359aded5dca8a4edea58ed67
Lev Levitsky <lev.levitsky@phystech.edu>
2013-03-20 17:04:41 +0400

    Fixes

--- tests/profiler.py
+++ tests/profiler.py
@@ -7,8 +7,9 @@ if len(sys.argv) > 1:
     num = int(sys.argv[1])
 else:
     num = 3
+settings = ip.settings('test.cfg')
 with mgf.read('swedcad.mgf') as r:
     cProfile.run(
-        '''for _, (s, res) in zip(range(num), ip.process_parallel(r, hyperscore, 1)):
+        '''for _, (s, res) in zip(range(num), ip.process_file(r, settings)):
             pass
         ''')
