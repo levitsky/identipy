eba374127b655c3f84fa3e73ac69d02d04395833
Lev Levitsky <3358268+levitsky@users.noreply.github.com>
2013-12-24 18:04:04 +0400

    Update the test, rename the missed cleavages setting

--- tests/checker.py
+++ tests/checker.py
@@ -1,12 +1,32 @@
 from identipy import main, utils
 from sys import argv
 
-settings = main.settings('test.cfg')
+inputfile = argv[1] if len(argv) > 1 else 'test.mgf'
+if inputfile == 'swedcad.mgf':
+    settings = main.settings('swedcad.cfg')
+    tot, wro, leu = 0, 0, 0
+    try:
+        for res in main.process_file(inputfile, settings):
+            tot += 1
+            pep = res['spectrum']['params']['title']
+            match = res['candidates'][0][1] if res['candidates'] else '<NOTHING>'
+            if pep != match:
+                wro += 1
+                if pep.replace('I', 'L') == match.replace('I', 'L'):
+                    leu += 1
+                else:
+                    print pep, '<=>', match
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print 'Search finished.'
+        if tot:
+            print 'FDR: {:.0f}% ({:.0f}% of them Leu/Ile swaps).'.format(
+                    100.0 * wro / tot, 100.0 * leu / tot)
+        else:
+            print 'No results.'
 
-try:
-    inputfile = argv[1] if len(argv) > 1 else 'test.mgf'
+else:
+    settings = main.settings('test.cfg')
     utils.write_pepxml(inputfile, settings, main.process_file(inputfile, settings))
-except KeyboardInterrupt:
-    pass
-finally:
-    print('The search is finished')
+    print 'The search is finished.'
