72dcf5231ef244e3a3f507847698f923c2738210
Lev Levitsky <3358268+levitsky@users.noreply.github.com>
2014-02-24 18:08:16 +0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+'''
+setup.py file for identipy
+'''
+
+from distutils.core import setup
+
+version = open('VERSION').readline().strip()
+
+setup(
+    name = 'identipy',
+    version = version,
+    description      = '''Pyteomics-based search engine''',
+    long_description = (''.join(open('README').readlines()) + '\n'
+                        + ''.join(open('INSTALL').readlines())),
+    author           = 'Lev Levitsky & Mark Ivanov',
+    author_email     = 'pyteomics@googlegroups.com',
+    url              = 'http://hg.theorchromo.ru/identipy',
+    packages         = ['identipy', ],
+    requires         = [line.strip() for line in open('requirements.txt')],
+    classifiers      = ['Intended Audience :: Science/Research',
+                        'Programming Language :: Python :: 2.7',
+                        'Topic :: Scientific/Engineering :: Bio-Informatics',
+                        'Topic :: Software Development :: Libraries'],
+    license          = 'License :: OSI Approved :: Apache Software License',
+    )
