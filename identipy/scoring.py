68cc5c05620f6e6955605026503d4cc78469d81b
Lev Levitsky <lev.levitsky@phystech.edu>
2015-09-23 18:18:32 +0300

    Use set fragment accuracy in get_fragment_mass_tol

--- identipy/scoring.py
+++ identipy/scoring.py
@@ -25,7 +25,7 @@ def simple_score(spectrum, peptide, settings):
 
 def get_fragment_mass_tol(spectrum, peptide, settings):
     """A function for obtaining optimal fragment mass tolerance, dynamic range"""
-    acc = 1.0  # maximum product accuracy
+    acc = settings.getfloat('search', 'product accuracy')
     spectrum = copy(spectrum)
     idx = np.nonzero(spectrum['m/z array'] >= 150)
     spectrum['intensity array'] = spectrum['intensity array'][idx]
