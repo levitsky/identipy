6c6b2b427410f3ff86ffbb491c2eed6736936b93
Lev Levitsky <lev.levitsky@phystech.edu>
2015-10-01 13:09:35 +0300

    First try with peptide-centric approach

--- tests/profiler.py
+++ tests/profiler.py
@@ -1,17 +1,10 @@
 from identipy import main as ip
-from identipy.scoring import hyperscore
-from pyteomics import mgf
-import sys
 import cProfile
-if len(sys.argv) > 1:
-    num = int(sys.argv[1])
-else:
-    num = 3
-settings = ip.settings('UPS2.cfg')
+
+settings = ip.settings('UPS3.cfg')
 if settings.getint('performance', 'processes') != 1:
     settings.set('performance', 'processes', 1)
     print "Setting the number of processes to 1."
 cProfile.run(
-    '''for _, res in zip(range(num), ip.process_file('test.mgf', settings)):
-        pass
+    '''r = list(ip.process_file('test.mgf', settings))
     ''', 'profiler.out')
