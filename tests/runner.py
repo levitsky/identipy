ec4997c916423281600d2673772244410ead4e32
Lev Levitsky <3358268+levitsky@users.noreply.github.com>
2017-06-22 13:29:33 +0300

    Forget unneeded files

--- tests/runner.py
+++
@@ -1,7 +0,0 @@
-from identipy import main, utils
-from sys import argv
-
-inputfile = argv[2]
-cfg = argv[1]
-settings = main.settings(cfg)
-utils.write_output(inputfile, settings, main.process_file(inputfile, settings))
