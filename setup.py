1ad0e5a743a1557b4b7a1ceb3362d4c3ebf10c67
Lev Levitsky <lev.levitsky@phystech.edu>
2017-08-05 01:17:06 +0300

    Fix in setup

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@
 '''
 setup.py file for identipy
 '''
-
 from setuptools import setup, find_packages
-
 version = open('VERSION').readline().strip()
 
 setup(
-    name = 'identipy',
-    version = version,
+    name             = 'identipy',
+    version          = version,
     description      = '''Pyteomics-based search engine''',
     long_description = (''.join(open('README.md').readlines()) + '\n'
                         + ''.join(open('INSTALL').readlines())),
@@ -18,12 +16,12 @@ setup(
     author_email     = 'pyteomics@googlegroups.com',
     url              = 'http://hg.theorchromo.ru/identipy',
     packages         = ['identipy', ],
-    package_data     = {'identipy': ['identipy/default.cfg']},
+    package_data     = {'identipy': ['default.cfg']},
     install_requires = [line.strip() for line in open('requirements.txt')],
     classifiers      = ['Intended Audience :: Science/Research',
                         'Programming Language :: Python :: 2.7',
                         'Topic :: Scientific/Engineering :: Bio-Informatics',
                         'Topic :: Software Development :: Libraries'],
     license          = 'License :: OSI Approved :: Apache Software License',
-    entry_points     = {'console_scripts': ['identipy = identipy.cli:main']}
+    entry_points     = {'console_scripts': ['identipy = identipy.cli:run']}
     )
