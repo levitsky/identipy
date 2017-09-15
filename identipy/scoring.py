0c6fd2f8a7afc2f658de7c87774e3b12b3e333fd
Mark Ivanov <markmipt@gmail.com>
2017-09-15 17:45:54 +0300

    Speed up multiprocessing

--- identipy/scoring.py
+++ identipy/scoring.py
@@ -182,12 +182,9 @@ def hyperscore(spectrum, theoretical, acc, acc_ppm=False, position=False):
 def RNHS_fast(spectrum_fastset, theoretical_set, min_matched):
     matched_approx_b = len(spectrum_fastset.intersection(theoretical_set['b']))
     matched_approx_y = len(spectrum_fastset.intersection(theoretical_set['y']))
-    # matched_approx = len(spectrum_fastset.intersection(theoretical_set))
     matched_approx = matched_approx_b + matched_approx_y
     if matched_approx >= min_matched:
         return matched_approx, factorial(matched_approx_b) * factorial(matched_approx_y)
-        # return matched_approx, factorial(matched_approx_b) * (100 * matched_approx_b) + factorial(matched_approx_y) * (100 * matched_approx_y)
-        # return matched_approx, factorial(matched_approx) * (100 * matched_approx)
     else:
         return 0, 0
 
