7c22ea007a6e47dc438d9b7ad040c38b9ab37aef
Lev Levitsky <lev.levitsky@phystech.edu>
2022-03-16 18:41:25 +0300

    Add test script

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd test_data
+find . -name '*.mgf' -print -exec identipy -cfg identipy.cfg {} \;
+pyteomics pepxml info *.pep.xml
+
+echo "Reference values (1% FDR):"
+echo 26718
+echo 17670
